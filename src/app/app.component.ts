35045a17ee19e7b82c0c3adf264a326c6be67999
MbhanuS <bhanusengar24@gmail.com>
2021-02-14 22:59:18 +0530

    generatingpwd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'randompasswordgenerator';
+  password = ''
+  passwordLength = 0
+  includeLetters = false
+  includeNumbers = false
+  includeSpecialChars = false
+
+  onProvidingLength(event: any) {
+    const parsedValue = parseInt(event.target.value)
+
+    if(!isNaN(parsedValue)) {
+      this.passwordLength = parsedValue
+    }
+  }
+
+  onIncludingLetters() {
+    this.includeLetters = !this.includeLetters
+  }
+  onIncludingNumbers() {
+    this.includeNumbers = !this.includeNumbers
+  }
+  onIncludingSpecialChars() {
+    this.includeSpecialChars = !this.includeSpecialChars
+  }
+
+  onGeneratingPassword() {
+    if(this.passwordLength < 1) {
+      return alert('length should be greater than 0')
+    }
+    if(!(this.includeLetters || this.includeNumbers || this.includeSpecialChars)) {
+      return alert('select at least 1 checkbox')
+    }
+    const letters = 'abcdefghijklmnopqrstuvwxyz'
+    const numbers = '0123456789'
+    const specialchars = '~!@#$%^&*()_  '
+    let validChars = ''
+    if(this.includeLetters) {
+      validChars += letters
+    }
+    if(this.includeNumbers) {
+      validChars += numbers
+    }
+    if(this.includeSpecialChars) {
+      validChars += specialchars
+    }
+
+    let generatedPassword = ''
+    for(let i =0; i<this.passwordLength; i++) {
+      const index = Math.floor(Math.random() * validChars.length)
+      generatedPassword += validChars[index]
+    }
+    this.password = generatedPassword
+  }
 }
